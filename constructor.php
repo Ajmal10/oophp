5607be99591e2549917e4b524b009b8d3a1c3755
Levandri Ajmal <levandriajmal@gmail.com>
2019-10-04 10:03:13 +0700

    menambahkan produk3 pada file cunstructor.php

--- constructor.php
+++ constructor.php
@@ -4,12 +4,12 @@
 // Komik
 // Game
 class Produk {
-	public  $judul = "judul",
-			$penulis = "penulis",
-			$penerbit = "penerbit",
-			$harga = 0;
+	public  $judul,
+			$penulis,
+			$penerbit,
+			$harga;
 
-	public function __construct( $judul, $penulis, $penerbit, $harga){
+	public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0){
 		$this->judul = $judul;
 		$this->penulis = $penulis;
 		$this->penerbit = $penerbit;
@@ -23,7 +23,11 @@ class Produk {
 
 $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
 $produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+$produk3 = new Produk("Dragon Ball");
+
 echo "Komik : " . $produk1->getLabel();
 echo "<br>";
 echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
  ?>
