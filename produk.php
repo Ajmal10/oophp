2bf6ad03eacc6787f344e3d6a2a187e2311bd084
Levandri Ajmal <levandriajmal@gmail.com>
2019-10-04 08:47:13 +0700

    Mengubah isi yg ada di file produk.php

--- produk.php
+++ produk.php
@@ -4,13 +4,32 @@
 // Komik
 // Game
 class Produk {
-	public  $judul,
-			$penulis,
-			$penerbit,
-			$harga;
+	public  $judul = "judul",
+			$penulis = "penulis",
+			$penerbit = "penerbit",
+			$harga = 0;
+
+	public function getLabel() {
+		return "$this->penulis, $this->penerbit";
+	}
 }
 
-$Produk1 = new Produk();
-var_dump($Produk1);
+// $Produk1 = new Produk();
+// $Produk1->judul = "Naruto";
+// var_dump($Produk1);
 
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+echo "Komik : $produk3->penulis, $produk3->penerbit";
+echo "<br>";
+echo $produk3->getLabel();
  ?>
