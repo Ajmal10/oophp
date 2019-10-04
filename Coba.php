d960355d70bdebdb65bc4cdf79f3b9ab256446f3
Levandri Ajmal <levandriajmal@gmail.com>
2019-10-04 08:01:22 +0700

    Menambahkan var_dump dan file Produk.php

--- Coba.php
+++ Coba.php
@@ -10,5 +10,6 @@ $b = new Coba();
 $c = new Coba();
 
 var_dump($a);
-
+var_dump($b);
+var_dump($c);
  ?>
---
+++ produk.php
@@ -0,0 +1,13 @@
+<?php 
+
+// Jualan Produk
+// Komik
+// Game
+class Produk {
+
+}
+
+$Produk1 = new Produk();
+$Produk2 = new Produk();
+
+ ?>
