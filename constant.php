cd33d9da6be2a32a3a6199133af63bb63790660a
Levandri Ajmal <levandriajmal@gmail.com>
2019-10-05 10:39:02 +0700

    Menambahkan magic constant pada file constant.php

--- constant.php
+++ constant.php
@@ -1,10 +1,28 @@
 <?php 
 
-class Coba {
-	const NAMA = 'Ajmal';
+// class Coba {
+// 	const NAMA = 'Ajmal';
 
+// }
+
+// echo Coba::NAMA;
+
+echo __LINE__;
+echo "<br>";
+echo __FILE__;
+echo "<br>";
+
+function coba() {
+	return __FUNCTION__;
 }
 
-echo Coba::NAMA;
+echo coba();
+echo "<br>";
+
+class Coba {
+	public $kelas = __CLASS__;
+}
+$obj = new Coba;
+echo $obj->kelas;
 
  ?>
