c9174a64b96238217d09a12d0063d65635c78687
Levandri Ajmal <levandriajmal@gmail.com>
2019-10-05 10:05:26 +0700

    Mengganti menjadi class Contoh dan menginstansiasi

--- static.php
+++ static.php
@@ -1,16 +1,38 @@
+
 <?php 
 
-class ContohStatic {
-	public static $angka =1;
+// class ContohStatic {
+// 	public static $angka =1;
 
-	public static function halo() {
-		return "Halo." . self::$angka . " kali.";
+// 	public static function halo() {
+// 		return "Halo." . self::$angka . " kali.";
+// 	}
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::halo();
+// echo "<hr>";
+// echo ContohStatic::halo();
+
+class Contoh {
+	public static $angka = 1;
+
+	public function halo() {
+		return "Halo " . self::$angka++ . " Kali. <br>";
 	}
 }
 
-echo ContohStatic::$angka;
-echo "<br>";
-echo ContohStatic::halo();
+$obj = new Contoh;
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
+
 echo "<hr>";
-echo ContohStatic::halo();
+
+$obj2 = new Contoh;
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
+
  ?>
